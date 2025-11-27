595528916e2031b5640ceb8f83e524a7adaad97a
c2461630 <137661544+c2461630@users.noreply.github.com>
2025-11-27 09:41:34 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,8 +7,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      // 告訴 Vite 這些模組是外部的，不要嘗試打包它們
-      // 瀏覽器會在執行時透過 index.html 的 importmap 找到它們
       external: [
         'firebase/app',
         'firebase/firestore'
